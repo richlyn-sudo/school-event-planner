a0ebb43bf1153b1f99076279e2e1f41c6bcb1cff
richlyn-sudo <richlynaraneta41@gmail.com>
2026-05-28 01:56:20 +0800

    update homepage UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <main style={{ padding: "40px", textAlign: "center" }}>
+      <h1>🎓 School Event Planner</h1>
+      <p>Welcome to your system</p>
+
+      <div style={{ marginTop: "20px" }}>
+        <a href="/login">Login</a> |{" "}
+        <a href="/register">Register</a>
+      </div>
+    </main>
+  );
 }
